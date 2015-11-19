752edad9d32cf7b531dfde67dd2edc21494730fb
michimussato@gmail.com <michimussato@gmail.com>
2015-11-19 08:48:56 +0000

    playing around with graphics scene

---
+++ src/modules/audioplayer/audioplayer.py
@@ -0,0 +1,186 @@
+import os
+from PyQt4 import QtGui
+from PyQt4 import uic
+# from PyQt4.QtGui import *
+# from PyQt4.uic import *
+from vlc import *
+
+
+class PlayerUi(QWidget):
+    def __init__(self, parent = None):
+        super(PlayerUi, self).__init__(parent)
+        self.pypelyne_root = os.getcwd()
+
+        self.ui = uic.loadUi(os.path.join(os.path.dirname(self.main_window.pypelyne_root), 'ui', 'player.ui'), self)
+        #self.ui = loadUi(r'C:\Users\michael.mussato.SCHERRERMEDIEN\Dropbox\development\workspace\PyPELyNE\ui\player.ui')
+
+        self.player = None
+
+        #self.create_connects()
+
+
+    def createConnects(self):
+        self.radioButtonPlay.toggled.connect(self.play)
+        self.radioButtonStop.toggled.connect(self.stop)
+
+    def play(self):
+        self.player = MediaPlayer(r'C:\Users\michael.mussato.SCHERRERMEDIEN\Dropbox\development\workspace\PyPELyNE\src\audio\cairnomount.mp3')
+        self.player.play()
+
+    def stop(self):
+        try:
+            self.player.stop()
+        except:
+            pass
+
+
+if __name__ == "__main__":
+
+    app = QApplication(sys.argv)
+    playerWindow = playerUi()
+    #screenSize = QApplication.desktop().availableGeometry()
+
+    playerUi.show()
+    app.exec_()
+
+
+
+    
+    # def add_player(self):
+    #     self.player_ui = PlayerWidgetUi(self)
+    #     self.horizontalLayout.addWidget(self.player_ui)
+    #     # self.player_ui.radioButtonPlay.clicked.connect(self.play_audio)
+    #     self.player_ui.pushButtonPlayStop.clicked.connect(self.play_audio)
+    #
+    #     self.player_ui.pushButtonPlayStop.setContextMenuPolicy(Qt.CustomContextMenu)
+    #     self.connect(self.player_ui.pushButtonPlayStop, SIGNAL('customContextMenuRequested(const QPoint&)'), self.player_context_menu)
+    #
+    #     self.player_context_menu = QMenu()
+    #     q_menu_titles = []
+    #
+    #     for dir, subdirs, files in os.walk(SETTINGS.AUDIO_FOLDER_DARWIN, topdown=False):
+    #         for file in files:
+    #             if file in SETTINGS.EXCLUSIONS:
+    #                 os.remove(os.path.join(dir, file))
+    #                 logging.warning('file %s deleted from %s' % (file, dir))
+    #
+    #             elif os.path.splitext(file)[1] not in SETTINGS.AUDIO_EXTENSIONS:
+    #                 logging.warning('non audio file %s found in %s' % (file, dir))
+    #
+    #             else:
+    #                 if not os.path.relpath(dir, SETTINGS.AUDIO_FOLDER_DARWIN) == '.':
+    #                     q_menu_name = os.path.relpath(dir, SETTINGS.AUDIO_FOLDER_DARWIN)
+    #                     # if len(q_menu_name.split(os.sep)) > 1:
+    #                     if q_menu_name not in q_menu_titles:
+    #                         self.menuAlbum = self.player_context_menu.addMenu(q_menu_name.replace(os.sep, ' - '))
+    #                         q_menu_titles.append(q_menu_name)
+    #
+    #                         self.menuAlbum.addAction(file, self.play_audio_callback(os.path.join(dir, file)))
+    #                         self.audio_folder_content.append(os.path.join(dir, file))
+    #                     else:
+    #                         self.menuAlbum.addAction(file, self.play_audio_callback(os.path.join(dir, file)))
+    #                         self.audio_folder_content.append(os.path.join(dir, file))
+    #
+    #                 else:
+    #                     self.player_context_menu.addAction(file, self.play_audio_callback(os.path.join(dir, file)))
+    #                     self.audio_folder_content.append(os.path.join(dir, file))
+    #
+    #     self.player_ui.pushButtonPlayStop.setText('play')
+    #     self.player_exists = False
+    #
+    # def player_context_menu(self, point):
+    #     self.player_context_menu.exec_(self.player_ui.pushButtonPlayStop.mapToGlobal(point))
+    #
+    # def play_audio_callback(self, track=None):
+    #     def callback():
+    #         self.play_audio(track)
+    #     return callback
+    #
+    # def play_audio(self, track=None):
+    #     # https://forum.videolan.org/viewtopic.php?t=107039
+    #     if len(os.listdir(SETTINGS.AUDIO_FOLDER_DARWIN)) == 0:
+    #         logging.warning('no audio files found')
+    #         self.player_ui.radioButtonPlay.setEnabled(False)
+    #
+    #     elif not self.player_exists:
+    #         random.shuffle(self.audio_folder_content, random.random)
+    #
+    #         if not track:
+    #             track_id = self.audio_folder_content.index(track)
+    #
+    #         self.mlp = MediaListPlayer()
+    #         self.mp = MediaPlayer()
+    #         self.mlp.set_media_player(self.mp)
+    #
+    #         self.ml = MediaList()
+    #
+    #         for file in self.audio_folder_content:
+    #             self.ml.add_media(os.path.join(SETTINGS.AUDIO_FOLDER_DARWIN, file))
+    #
+    #         self.mlp.set_media_list(self.ml)
+    #
+    #         if track:
+    #             self.mlp.play_item_at_index(track_id)
+    #             logging.info('playing %s' % track_id)
+    #
+    #         else:
+    #             self.mlp.play()
+    #             logging.info('playing randomly')
+    #
+    #         self.player_exists = True
+    #
+    #         self.player_ui.pushButtonPlayStop.clicked.disconnect(self.play_audio)
+    #         self.player_ui.pushButtonPlayStop.clicked.connect(self.stopAudio)
+    #         self.player_ui.pushButtonPlayStop.setText('stop')
+    #         logging.info('setting pushButtonPlayStop function to stop')
+    #         threading.Timer(0.5, self.from_stop_to_skip).start()
+    #
+    #     elif self.player_exists and track:
+    #         logging.info('playing %s' % track)
+    #         track_id = self.audio_folder_content.index(track)
+    #         self.skip_audio(track_id)
+    #
+    #     else:
+    #         logging.info('already on air')
+    #
+    # def from_stop_to_skip(self):
+    #     if self.player_exists:
+    #         self.player_ui.pushButtonPlayStop.clicked.disconnect(self.stopAudio)
+    #         self.player_ui.pushButtonPlayStop.clicked.connect(self.skip_audio)
+    #         self.player_ui.pushButtonPlayStop.setText('skip')
+    #         logging.info('setting pushButtonPlayStop function to skip')
+    #
+    # def from_skip_to_stop(self):
+    #     if self.player_exists:
+    #         self.player_ui.pushButtonPlayStop.clicked.disconnect(self.skip_audio)
+    #         self.player_ui.pushButtonPlayStop.clicked.connect(self.stopAudio)
+    #         self.player_ui.pushButtonPlayStop.setText('stop')
+    #         logging.info('setting pushButtonPlayStop function to stop')
+    #
+    # def stop_audio(self):
+    #     if self.player_exists:
+    #         try:
+    #             self.player_ui.pushButtonPlayStop.clicked.disconnect(self.stopAudio)
+    #             self.player_ui.pushButtonPlayStop.clicked.connect(self.play_audio)
+    #             self.mp.stop()
+    #             self.mp.release()
+    #             self.mlp.release()
+    #             self.player_exists = False
+    #             self.player_ui.pushButtonPlayStop.setText('play')
+    #             logging.info('setting pushButtonPlayStop function to play')
+    #             #logging.info('audio stopped')
+    #         except:
+    #             logging.warning('error or not playing')
+    #
+    # def skip_audio(self, track_id=None):
+    #     if self.player_exists:
+    #         if track_id:
+    #             self.mlp.play_item_at_index(track_id)
+    #
+    #         else:
+    #             self.mlp.next()
+    #
+    #         self.player_ui.pushButtonPlayStop.clicked.disconnect(self.skip_audio)
+    #         self.player_ui.pushButtonPlayStop.clicked.connect(self.stopAudio)
+    #         self.player_ui.pushButtonPlayStop.setText('stop')
+    #         threading.Timer(0.5, self.from_stop_to_skip).start()
---
+++ src/modules/graphicsscene/graphicsscene.py
@@ -0,0 +1,185 @@
+import os
+from PyQt4 import QtGui
+from PyQt4 import QtCore
+from PyQt4 import uic
+import src.conf.settings.SETTINGS as SETTINGS
+
+
+class GraphicsView(QtGui.QGraphicsView):
+    def __init__(self, myView=[]):
+        super(GraphicsView, self).__init__()
+        # self.ui = uic.loadUi(os.path.join(SETTINGS.PYPELYNE2_ROOT, 'src', 'modules', 'graphicsscene', 'graphicsscene.ui'), self)
+        self.setResizeAnchor(QtGui.QGraphicsView.AnchorUnderMouse)
+        self.setTransformationAnchor(QtGui.QGraphicsView.AnchorUnderMouse)
+        # self.nodeView.wheelEvent = self.graphicsView_wheelEvent
+        self.myView = myView
+
+        self.setGeometry(600, 300, 600, 400)
+
+        self.sc = GraphicsScene(parent=self)
+        self.setScene(self.sc)
+
+    # def wheelEvent(self, event):
+    #     # 'zoom'
+    #     sc = event.delta()/100
+    #     if sc < 0:
+    #         sc -= 1/sc
+    #     self.myView.scale(sc, sc)
+    #     self.myView.setDragMode(0)
+    #     self.myView.showMatrix()
+
+
+class GraphicsScene(QtGui.QGraphicsScene):
+    def __init__(self, parent=None):
+        super(GraphicsScene, self).__init__(parent)
+        self.addRect(30, 16, 20, 20, pen=QtGui.QPen(QtCore.Qt.blue))
+
+
+
+
+
+
+#     def graphicsView_wheelEvent(self, event):
+#
+# #         numSteps = event.delta() / 15 / 8
+# #
+# #         if numSteps == 0:
+# #             event.ignore()
+# #
+# #         sc = 1.25 * numSteps
+# #         self.zoom(sc, self.mapToScene(event.pos()))
+# #         event.accept()
+#
+#         factor = 1.15
+#
+#         #self.nodeView.centerOn()
+#
+#         #print 'event.delta() = %s' %event.delta()
+#
+#         if event.delta() > 0:
+#             self.nodeView.scale(factor, factor)
+#             self.nodeView.centerOn(event.pos())
+#
+#         else:
+#             self.nodeView.scale(1.0 / factor, 1.0 / factor)
+#             self.nodeView.centerOn(event.pos())
+
+
+# '''Pan (left mouse but) and zoom (mouse wheel)  test'''
+# '''Python 3.4 (No differance between PyQt4 and PyQt5)'''
+# #from PyQt5 import QtGui, QtCore
+# #import PyQt5.QtWidget as QW
+# from PyQt4 import QtGui, QtCore
+# import PyQt4.QtGui as QW
+#
+# import numpy as np
+#
+# class ZoomAndPan(QW.QGraphicsView):
+#
+#     def __init__(self,parent=None):
+#         super(ZoomAndPan,self).__init__(parent)
+#         self.setWindowTitle('ZoomAndPan')
+#         self.setGeometry(600,300,600,400)
+#         'Left button in top of image. Shows the transform matrix (press to reset)'
+#         self.mess = QW.QPushButton('Transform Matrix\n\n\ndx,xy\ncounter', self)
+#         self.mess.clicked.connect(self.resetM)
+#
+#         'm31 button, adds 200 to m31'
+#         self.m31 = QW.QPushButton('m31', self)
+#         self.m31.move(200,0)
+#         self.m31.clicked.connect(self.addM31)
+#
+#         'm13 button, adds 0.0001 to m13'
+#         self.m13 = QW.QPushButton('m13', self)
+#         self.m13.move(300,0)
+#         self.m13.clicked.connect(self.addM13)
+#         self.count=0   #Counter
+#
+#         # Create scene
+#         self.sc = scene(self,self)
+#         self.setScene(self.sc)
+#
+#     def mouseMoveEvent(self,event):
+#         'Pan by manipulting sceneRect'
+#         pos=event.pos()
+#         pos=self.mapToScene(pos)
+#         dx=pos.x()-self.sc.startPos.x()
+#         dy=pos.y()-self.sc.startPos.y()
+#
+#         rect=self.sceneRect().getRect()
+#         self.setSceneRect(rect[0]-dx,rect[1]-dy,rect[2],rect[3])
+#         # Increas counter to show that the loop works
+#         self.count+=1
+#         self.showMatrix()
+#
+#     def showMatrix(self):
+#         'Show matrix in Textbox (Buttton)'
+#         m=self.transform()
+#         str1='{0:5.2f}{1:5.2f} {2:6.4f}\n'.format(m.m11(), m.m12(),m.m13())
+#         str2='{0:5.2f}{1:5.2f}{2:5.2f}\n'.format(m.m21(), m.m22(),m.m23())
+#         str3='{0:5.2f}{1:5.2f}{2:5.2f}\n'.format(m.m31(), m.m32(),m.m33())
+#         str4='{0:5.2f}{1:5.2f}\n'.format(m.dx(), m.dy(),m.m33())
+#         'Show counter '
+#         str5='{0:5.0f}'.format(self.count)
+#         self.mess.setText(str1+str2+str3+str4+str5)
+#
+#     def resetM(self):
+#         'Reset transform'
+#         self.resetTransform()
+#         self.showMatrix()
+#
+#     def addM31(self):
+#         'Add 200 to m31 '
+#         m=self.transform()
+#         m.setMatrix(m.m11(),m.m12(),m.m13(),m.m21(),m.m22(),m.m23(),m.m31()+200,m.m32(),m.m33())
+#         self.setTransform(m)
+#         self.showMatrix()
+#
+#     def addM13(self):
+#         'Add 0.0001 to m13 '
+#         m=self.transform()
+#         m.setMatrix(m.m11(),m.m12(),m.m13()+0.0001,m.m21(),m.m22(),m.m23(),
+#             m.m31(),m.m32(),m.m33())
+#         self.setTransform(m)
+#         self.showMatrix()
+#
+# class scene(QW.QGraphicsScene):
+#     def __init__(self,parent,myView=[]):
+#         QW.QGraphicsScene.__init__(self,parent)
+#         self.myView=myView
+#         # Some items in scene
+#         self.txt=self.addSimpleText("///////")
+#         self.txt.setPos(2,-20)
+#         self.txt.setScale(2)
+#         self.txt.setBrush(QtGui.QBrush(QtCore.Qt.green))
+#         self.addRect(0,16,20,20, pen=QtGui.QPen(QtCore.Qt.blue))
+#         self.addRect(10,60,32,8, pen=QtGui.QPen(QtCore.Qt.red))
+#         self.addRect(30,16,20,20, pen=QtGui.QPen(QtCore.Qt.blue))
+#         self.N=0
+#
+#     def mousePressEvent(self, event):
+#         self.myView.setDragMode(1) # Works fine without this
+#         self.startPos=event.scenePos()
+#
+#     def mouseReleaseEvent(self, event):
+#         self.myView.setDragMode(0)
+#
+#     def wheelEvent(self, event):
+#         'zoom'
+#         sc=event.delta()/100
+#         if sc<0: sc=-1/sc
+#         self.myView.scale(sc,sc)
+#         self.myView.setDragMode(0)
+#         self.myView.showMatrix()
+#
+
+
+def main():
+    app = QtGui.QApplication([])
+    fig = GraphicsView()
+    fig.show()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    main()
