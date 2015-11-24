2b03b0e0340cf03f65bdcafdc81435958cf63dd9
Michael Mussato <michaelmussato@axisanimation.com>
2015-11-24 16:12:08 +0000

    splash can be moved to mainwindow file as well

--- src/modules/ui/mainwindow/test_mainwindow.py
+++ src/modules/ui/mainwindow/test_mainwindow.py
@@ -10,3 +10,16 @@ scene = mainwindow.MainWindow()
 # scene.resize(int(screen_size.width()), int(screen_size.height()))
 scene.show()
 app.exec_()
+
+# app = QtGui.QApplication(sys.argv)
+#
+# splash = QtGui.QPixmap(ICON_DEFAULT)
+# splash = QtGui.QSplashScreen(splash, QtCore.Qt.WindowStaysOnTopHint)
+# splash.setMask(splash.mask())
+# splash.show()
+# app.processEvents()
+#
+# from applauncher_ui import AppLauncherUI
+# app_launcher = AppLauncherUI(studio='')
+# splash.finish(app_launcher)
+# return app.exec_()
