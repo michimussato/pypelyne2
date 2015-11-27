a893e7cd8de8c296b58123f97d402c05650cad5e
michimussato@gmail.com <michimussato@gmail.com>
2015-11-27 01:10:31 +0000

    cleanup

--- src/modules/ui/dockwidget/dockwidget_output.py
+++ src/modules/ui/dockwidget/dockwidget_output.py
@@ -1,13 +1,9 @@
-import logging
 import datetime
 
-from PyQt4 import QtGui
 from PyQt4 import QtCore
 
 import src.modules.ui.dockwidget.dockwidget as dockwidget
 import src.modules.ui.pluginconsole.pluginconsole as pluginconsole
-
-import src.conf.settings.SETTINGS as SETTINGS
 
 
 class DockWidgetOutput(dockwidget.DockWidget):
@@ -26,63 +22,19 @@ class DockWidgetOutput(dockwidget.DockWidget):
         self.setWidget(self.console)
 
     def data_ready_std(self, process):
-    # def data_ready_std(self, process, process_color):
         box = self.console.ui.output_area_std
         cursor_box = box.textCursor()
         cursor_box.movePosition(cursor_box.End)
-
-        # std_format = cursor_box.charFormat()
-        # new_format = cursor_box.charFormat()
-
-        # std_format.setBackground(QtCore.Qt.white)
-        # std_format.setForeground(QtCore.Qt.black)
-
-        # modify it
-        # new_format.setBackground(process_color)
-        # new_format.setForeground(process_color.lighter(160))
-        # apply it
-        # cursor_box.setCharFormat(new_format)
-
-        cursor_box.insertText('%s (std):   %s' % (datetime.datetime.now(), str(process.readAllStandardOutput())))
-        # logging.info('%s (std):   %s' % (datetime.datetime.now(), str(process.readAllStandardOutput())))
-
+        cursor_box.insertText('{0} (std):   {1}'.format(datetime.datetime.now(), str(process.readAllStandardOutput())))
         cursor_box.movePosition(cursor_box.End)
-        # char_format = cursor_box.charFormat()
-        # char_format.setBackground(QtCore.Qt.white)
-        # char_format.setForeground(QtCore.Qt.black)
-        # cursor_box.setCharFormat(std_format)
-
         cursor_box.insertText('\n')
-
         box.ensureCursorVisible()
 
     def data_ready_err(self, process):
-    # def data_ready_std(self, process, process_color):
         box = self.console.ui.output_area_err
         cursor_box = box.textCursor()
         cursor_box.movePosition(cursor_box.End)
-
-        # std_format = cursor_box.charFormat()
-        # new_format = cursor_box.charFormat()
-
-        # std_format.setBackground(QtCore.Qt.white)
-        # std_format.setForeground(QtCore.Qt.black)
-
-        # modify it
-        # new_format.setBackground(process_color)
-        # new_format.setForeground(process_color.lighter(160))
-        # apply it
-        # cursor_box.setCharFormat(new_format)
-
-        cursor_box.insertText('%s (err):   %s' % (datetime.datetime.now(), str(process.readAllStandardOutput())))
-        # logging.info('%s (err):   %s' % (datetime.datetime.now(), str(process.readAllStandardOutput())))
-
+        cursor_box.insertText('{0} (err):   {1}'.format(datetime.datetime.now(), str(process.readAllStandardOutput())))
         cursor_box.movePosition(cursor_box.End)
-        # char_format = cursor_box.charFormat()
-        # char_format.setBackground(QtCore.Qt.white)
-        # char_format.setForeground(QtCore.Qt.black)
-        # cursor_box.setCharFormat(std_format)
-
         cursor_box.insertText('\n')
-
         box.ensureCursorVisible()
--- src/modules/ui/mainwindow/test_mainwindow.py
+++ src/modules/ui/mainwindow/test_mainwindow.py
@@ -7,9 +7,9 @@ import src.conf.settings.SETTINGS as SETTINGS
 
 app = QtGui.QApplication(sys.argv)
 app.setWindowIcon(QtGui.QIcon(SETTINGS.SPLASH_ICON))
-# screen_size = QtGui.QApplication.desktop().availableGeometry()
+screen_size = QtGui.QApplication.desktop().availableGeometry()
 scene = mainwindow.MainWindow()
-# scene.resize(int(screen_size.width()), int(screen_size.height()))
+scene.resize(int(screen_size.width()), int(screen_size.height()))
 scene.show()
 scene.raise_()
 app.exec_()
